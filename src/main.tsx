7542e69e525bad9be7ea3db45d9c72e5cde45639
Arnas Rumbavicius <a.rumbavicius@gmail.com>
2024-12-15 15:30:34 +0100

    Non traditional oauth works

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,16 @@
 import './index.css'
 // @deno-types="@types/react"
-import { StrictMode } from 'react'
+import {StrictMode} from 'react'
 // @deno-types="@types/react-dom/client"
-import { createRoot } from 'react-dom/client'
+import {createRoot} from 'react-dom/client'
+// @ts-ignore
 import App from './App.tsx'
+import { GoogleOAuthProvider } from "@react-oauth/google"
 
 createRoot(document.getElementById('root') as HTMLElement).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+    <GoogleOAuthProvider clientId={"733167968471-7runi5g0s0gahprbah0lj1460ua2jjv3.apps.googleusercontent.com"}>
+        <StrictMode>
+            <App/>
+        </StrictMode>
+    </GoogleOAuthProvider>
 )
