013b9446f3837ac57420519945d7cbb122011907
Arnas Rumbavicius <a.rumbavicius@gmail.com>
2025-09-02 10:47:55 +0200

    Added custom footer to form

--- src/components/AbstractForm.tsx
+++ src/components/AbstractForm.tsx
@@ -7,39 +7,41 @@ import { Input } from "@chakra-ui/react";
 interface FormProps{
     fields: Array<InputField>,
     handleSubmit: (event: React.FormEvent) => void,
+    footer?: React.ReactNode,
 }
 
-function Form({fields, handleSubmit}: FormProps) {
-
+function Form({fields, handleSubmit, footer}: FormProps) {
    return(
-       <>
-           <form onSubmit={handleSubmit} id="form">
-               {
-                   fields.map((field: InputField, index: number) => {
-                       if(field.type == "image") {
-                            return(
-                                <ImageField
-                                    key={field.name || index}
-                                    name="image"
-                                />
-                            );
-                       } else {
-                           return (
-                               <label key={field.name || index}>
-                                   {field.label}
-                                   <Input
-                                       type={field.type} 
-                                       name={field.name} 
-                                       required={field.required}
-                                       accept={field.accept} 
-                                   />
-                               </label>
-                           )
-                       }})
-               }
-               <ReusableButton type="submit">Submit</ReusableButton>
-           </form>
-       </>
+       <form onSubmit={handleSubmit} id="form">
+           {
+               fields.map((field: InputField, index: number) => {
+                   if(field.type == "image") {
+                        return(
+                            <ImageField
+                                key={field.name || index}
+                                name="image"
+                            />
+                        );
+                   } else {
+                       return (
+                           <label key={field.name || index}>
+                               {field.label}
+                               <Input
+                                   type={field.type}
+                                   name={field.name}
+                                   required={field.required}
+                                   accept={field.accept}
+                               />
+                           </label>
+                       )
+                   }})
+           }
+           { footer && (
+               <div>
+                   {footer}
+               </div>
+           )}
+       </form>
    );
 }
 
