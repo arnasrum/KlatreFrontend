a54450bc35b48b2030992722aa13ba605ebde577
Arnas Rumbavicius <a.rumbavicius@gmail.com>
2025-10-07 00:22:01 +0200

    Removed old google auth dependency

--- src/main.tsx
+++ src/main.tsx
@@ -4,22 +4,19 @@ import {StrictMode} from 'react'
 // @deno-types="@types/react-dom/client"
 import {createRoot} from 'react-dom/client'
 import App from './App.tsx'
-import { GoogleOAuthProvider } from "@react-oauth/google"
 import { CookiesProvider} from "react-cookie";
 import { Provider } from "./components/ui/provider.tsx";
 import {ChakraProvider} from "@chakra-ui/react";
 import { system } from "./ChakraUIThemeConfig.ts"
 
 createRoot(document.getElementById('root') as HTMLElement).render(
-    <GoogleOAuthProvider clientId={"733167968471-7runi5g0s0gahprbah0lj1460ua2jjv3.apps.googleusercontent.com"}>
-        <CookiesProvider>
-            <Provider>
-                <StrictMode>
-                    <ChakraProvider value={system}>
-                        <App/>
-                    </ChakraProvider>
-                </StrictMode>
-            </Provider>
-        </CookiesProvider>
-    </GoogleOAuthProvider>
+    <CookiesProvider>
+        <Provider>
+            <StrictMode>
+                <ChakraProvider value={system}>
+                    <App/>
+                </ChakraProvider>
+            </StrictMode>
+        </Provider>
+    </CookiesProvider>
 )
