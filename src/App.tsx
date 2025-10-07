d91bcaf55faf5654d4a7d89d0eeeebc69e15ab40
Arnas Rumbavicius <a.rumbavicius@gmail.com>
2025-10-07 19:39:26 +0200

    Added back session context provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import UserContextProvider from "./contexts/UserContextProvider.tsx"
+import SessionContextProvider from "./contexts/SessionContext.tsx"
 import NavBar from "./components/NavBar.tsx"
 import Home from "./pages/Home.tsx"
 import Groups from "./pages/Groups.tsx"
@@ -9,15 +10,17 @@ import Group from "./pages/Group.tsx"
 function App() {
     return (
         <UserContextProvider>
-            <Router>
-                <NavBar />
-                <Routes>
-                    <Route path="/" element={<Home />} />
-                    <Route path="/groups" element={<Groups />} />
-                    <Route path="/groups/:uuid" element={<Group />} />
-                    <Route path="/login" element={<Login />} />
-                </Routes>
-            </Router>
+            <SessionContextProvider>
+                <Router>
+                    <NavBar />
+                    <Routes>
+                        <Route path="/" element={<Home />} />
+                        <Route path="/groups" element={<Groups />} />
+                        <Route path="/groups/:uuid" element={<Group />} />
+                        <Route path="/login" element={<Login />} />
+                    </Routes>
+                </Router>
+            </SessionContextProvider>
         </UserContextProvider>
     )
 }
